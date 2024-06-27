cdeb71e18f4ca926d4ab494cf891b32891e3db29
jairoalvarez14 <105599777+jairoalvarez14@users.noreply.github.com>
2024-06-26 20:10:44 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,27 @@
-import app from './server/app.js';
+import express from 'express';
 import http from 'http';
 import { connectDB } from './config/db.js';
 import { Server as SocketIOServer } from 'socket.io';
 import { TempData } from './models/data.model.js';
-import { SerialPort } from 'serialport';
-import { DelimiterParser } from '@serialport/parser-delimiter';
-import express from 'express';
-
 
 // Conectar a MongoDB
 connectDB();
 
+const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
     cors: {
-        origin: "*",
+        origin: "*", // Permitir todas las conexiones temporalmente
         methods: ["GET", "POST"]
     }
 });
 
 const PORT = process.env.PORT || 4000;
+
 app.use(express.json());
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-});
-
-// SERIAL COMMUNICATION
-const port = new SerialPort({
-    // Cambiar puerto de entrada de datos
-    path: '/dev/cu.usbmodem101',
-    // path: '/dev/cu.usbserial-10',
-    baudRate: 9600
-});
-
-const parser = port.pipe(new DelimiterParser({ delimiter: '\n' }));
-
-parser.on('open', () => {
-    console.log('Connection is opened');
-});
-
-port.on('error', (err) => {
-    console.log(err);
 });
 
 app.post('/', async (req, res) => {
@@ -57,11 +37,19 @@ app.post('/', async (req, res) => {
 
         // Emitir datos a través de Socket.IO
         io.emit('currentTemperature', temperature);
-        //io.emit('currentHumidity', humidity);
+        io.emit('currentHumidity', humidity);
 
+        console.log('Data received and emitted:', { temperature, humidity });
         res.status(200).send('Data received and processed');
     } catch (error) {
         console.error('Error processing data:', error);
         res.status(500).send('Server error');
     }
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
