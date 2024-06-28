f584d9328fa1fad1ec028ec442eaf94ebf144370
jairoalvarez14 <105599777+jairoalvarez14@users.noreply.github.com>
2024-06-27 23:12:30 -0600

    Update TempDataPage.js

--- frontend/src/components/TempDataPage.js
+++ frontend/src/components/TempDataPage.js
@@ -15,38 +15,42 @@ const TempDataPage = () => {
   }, []);
 
   return (
-    <div className="container__table">
-      <h2>Historial de Datos</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Plant</th>
-            <th>Temperature (°C)</th>
-            <th>Humidity (%)</th>
-            <th>Feels Like (%)</th>
-            <th>Frost Risk (%)</th>
-            <th>Timestamp</th>
-          </tr>
-        </thead>
-        <tbody>
-          {allData.length > 0 ? (
-            allData.map((data, index) => (
-              <tr key={index}>
-                <td>Lettuce</td>
-                <td>{data.temperature}</td>
-                <td>{data.humidity}</td>
-                <td>{data.feelsLike}</td>
-                <td>0</td>
-                <td>{data.timestamp}</td>
-              </tr>
-            ))
-          ) : (
+    <div>
+      <div className='title__container'>
+        <h2>Previous Data</h2>
+      </div>
+      <div className="container__table">
+        <table>
+          <thead>
             <tr>
-              <td colSpan="3">No data available</td>
+              <th>Plant</th>
+              <th>Temperature (°C)</th>
+              <th>Humidity (%)</th>
+              <th>Feels Like (%)</th>
+              <th>Frost Risk (%)</th>
+              <th>Timestamp</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {allData.length > 0 ? (
+              allData.map((data, index) => (
+                <tr key={index}>
+                  <td>Lettuce</td>
+                  <td>{data.temperature}</td>
+                  <td>{data.humidity}</td>
+                  <td>{data.feelsLike}</td>
+                  <td>0</td>
+                  <td>{data.timestamp}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3">No data available</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
