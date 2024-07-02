fed6a6eb7e4b886b99c6509a94e0a4b60c1b6b94
jairoalvarez14 <105599777+jairoalvarez14@users.noreply.github.com>
2024-07-02 08:55:56 -0600

    Update Home.js - Preview real data

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,84 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import { io } from 'socket.io-client';
 
 const Home = () => {
-  const [currentTemperature, setCurrentTemperature] = useState(0);
-  const [currentHumidity, setCurrentHumidity] = useState(0);
-  const [currentFeelsLike, setCurrentFeelsLike] = useState(0);
+  const [allData, setAllData] = useState([]);
 
   useEffect(() => {
-    const socket = io('https://ioturbanfarm-1.onrender.com', {
-      transports: ['websocket'],
-      secure: true,
-      reconnection: true,
-      reconnectionAttempts: Infinity,
-      reconnectionDelay: 1000,
-      reconnectionDelayMax: 5000,
-    });
-
-    socket.on('connect', () => {
-      console.log('Connected to the server');
-    });
-
-    socket.on('currentTemperature', (temp) => {
-      setCurrentTemperature(temp);
-    });
-
-    socket.on('currentHumidity', (hum) => {
-      setCurrentHumidity(hum);
-    });
-
-    socket.on('currentFeelsLike', (feelsLike) => {
-      setCurrentFeelsLike(feelsLike); 
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Disconnected from the server');
-    });
-
-    return () => {
-      socket.disconnect();
-    };
+    fetch("https://ioturbanfarm.onrender.com/allData")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data showen in real time!", data);
+        setAllData(data);
+      })
+      .catch((error) => console.error("Error obtaining data:", error));
   }, []);
-
-  return (
-    <div className="container-all">
-      <div className="card">
-        <div className="card-description">
-          <h3>Current Temperature</h3>
-          <p id="currentTemperature">{currentTemperature}°C</p>
-        </div>
-      </div>
-
-      <div className="humidity card">
-        <div className="card-description">
-          <h3>Humidity</h3>
-          <p id="currentHumidity">{currentHumidity}%</p>
-        </div>
-      </div>
     
-      <div className="feelsLike card">
-        <div className="card-description">
-          <h3>Feels like</h3>
-          <p id="feelsLike">{currentFeelsLike}%</p>
-        </div>
-      </div>
+  return (
+    <>
+      {allData.length > 0 ? (
+        allData.map((data, index) => (
+          <div className="container-all">
+            <div className="card">
+              <div className="card-description">
+                <h3>Current Temperature</h3>
+                <p id="currentTemperature">{data.temperature}°C</p>
+              </div>
+            </div>
 
-      <div className="frostRisk card">
-        <div className="card-description">
-          <h3>Frost Risk</h3>
-          {/* <p id="frostRisk">{frostRisk}%</p> */}
-          <p id="frostRisk">0%</p>
-        </div>
-      </div>
+            <div className="humidity card">
+              <div className="card-description">
+                <h3>Humidity</h3>
+                <p id="currentHumidity">{data.humidity}%</p>
+              </div>
+            </div>
 
-      <div className="bomb card">
-        <div className="card-description">
-          <h3>Bomb Status</h3>
-          <p id="bombStatus">Status Not Available</p>
-        </div>
-      </div>
-    </div>
+            <div className="feelsLike card">
+              <div className="card-description">
+                <h3>Feels like</h3>
+                <p id="feelsLike">{data.feelsLike}%</p>
+              </div>
+            </div>
+
+            <div className="frostRisk card">
+              <div className="card-description">
+                <h3>Frost Risk</h3>
+                {/* <p id="frostRisk">{data.frostRisk}%</p> */}
+                <p id="frostRisk">0%</p>
+              </div>
+            </div>
+
+            <div className="bomb card">
+              <div className="card-description">
+                <h3>Bomb Status</h3>
+                <p id="bombStatus">Status Not Available</p>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h2>No data available</h2>
+      )}
+    </>
   );
 };
 
