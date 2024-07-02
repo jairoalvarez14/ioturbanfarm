e3cbea4c680fcd19b531d26a0505ce60e64eb3d5
jairoalvarez14 <105599777+jairoalvarez14@users.noreply.github.com>
2024-07-02 09:47:23 -0600

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://ioturbanfarm.onrender.com");
+const socket = io("wss://ioturbanfarm.onrender.com");
 
 const Home = () => {
   const [lastData, setLastData] = useState({
