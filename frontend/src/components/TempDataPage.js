71196c7e2ddfffe36349323a391b3a60c358774a
jairoalvarez14 <105599777+jairoalvarez14@users.noreply.github.com>
2024-09-03 16:53:52 -0600

    Update TempDataPage.js
    Adding limit page

--- frontend/src/components/TempDataPage.js
+++ frontend/src/components/TempDataPage.js
@@ -1,58 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 const TempDataPage = () => {
-  const [allData, setAllData] = useState([]);
+    const [allData, setAllData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
 
-  useEffect(() => {
-    // Cambiar la dirección IP
-    fetch('https://ioturbanfarm.onrender.com/allData')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Datos recibidos:', data);
-        setAllData(data);
-      })
-      .catch(error => console.error('Error al obtener los datos:', error));
-  }, []);
+    useEffect(() => {
+        fetch(`https://ioturbanfarm.onrender.com/allData?page=${currentPage}&limit=25`)
+            .then(res => res.json())
+            .then(datos => {
+                console.log('Data receibed:', datos);
+                setData(datos.data);
+                setTotalPages(datos.totalPages);
+            })
+            .catch(error => console.error('Error:', err));
+    }, [currentPage]);
 
-  return (
-    <div>
-      <div className='title__container'>
-        <h2>Previous Data</h2>
-      </div>
-      <div className="container__table">
-        <table>
-          <thead>
-            <tr>
-              <th>Plant</th>
-              <th>Temperature (°C)</th>
-              <th>Humidity (%)</th>
-              <th>Feels Like (%)</th>
-              <th>Frost Risk (%)</th>
-              <th>Timestamp</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allData.length > 0 ? (
-              allData.map((data, index) => (
-                <tr key={index}>
-                  <td>Lettuce</td>
-                  <td>{data.temperature}</td>
-                  <td>{data.humidity}</td>
-                  <td>{data.feelsLike}</td>
-                  <td>0</td>
-                  <td>{data.timestamp}</td>
+    return (
+        <div>
+        <div className='title__container'>
+            <h2>Previous Data</h2>
+        </div>
+        <div className="container__table">
+            <table>
+            <thead>
+                <tr>
+                <th>Plant</th>
+                <th>Temperature (°C)</th>
+                <th>Humidity (%)</th>
+                <th>Feels Like (%)</th>
+                <th>Frost Risk (%)</th>
+                <th>Timestamp</th>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="3">No data available</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+            </thead>
+            <tbody>
+                {allData.length > 0 ? (
+                allData.map((data, index) => (
+                    <tr key={index}>
+                    <td>Lettuce</td>
+                    <td>{data.temperature}</td>
+                    <td>{data.humidity}</td>
+                    <td>{data.feelsLike}</td>
+                    <td>0</td>
+                    <td>{data.timestamp}</td>
+                    </tr>
+                ))
+                ) : (
+                <tr>
+                    <td colSpan="3">No data available</td>
+                </tr>
+                )}
+            </tbody>
+            </table>
+        </div>
+        </div>
+    );
 };
 
 export default TempDataPage;
